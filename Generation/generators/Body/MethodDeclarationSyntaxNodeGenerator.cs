ece6f4fe0cca11ae86e39159f91ff027ccd9d3f4
Michael Epps <mep053@gmail.com>
2022-03-03 06:34:00 -0500

    Readd deleted classes

---
+++ Generation/generators/Body/MethodDeclarationSyntaxNodeGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Generation.Generators.Stmt;
+using Generation.Generators.Types;
+using Generation.Java.Nodes;
+using Generation.Java.Nodes.Members;
+using Generation.Java.Nodes.Statements;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editing;
+
+namespace Generation.Generators.Body
+{
+    public class MethodDeclarationSyntaxNodeGenerator : ISyntaxNodeGenerator<MethodDeclaration>
+    {
+        public SyntaxNode Generate(SyntaxGenerator syntaxGenerator, MethodDeclaration node)
+        {
+            var parameters = node.Parameters?.Select(parameter =>
+                new ParameterSyntaxNodeGenerator().Generate(syntaxGenerator, parameter));
+            
+            var returnType = SyntaxFactory.ParseTypeName(node.JavaType.Identifier());
+            var accessibility = SyntaxNodeGeneratorHelpers.AccessibilityFromModifiers(node.Modifiers);
+            var declarationModifiers = SyntaxNodeGeneratorHelpers.DeclarationModifiersFromModifier(node.Modifiers);
+
+            var block = new BlockStatementSyntaxNodeGenerator().Generate(syntaxGenerator, node.Body) as BlockSyntax;
+            return syntaxGenerator.MethodDeclaration(node.SimpleName.Identifier, parameters, null, returnType, accessibility,
+                declarationModifiers, block?.Statements);
+            
+        }
+    }
+}
